c2b0f2192092d5c44351061989c7952524fd8272
LubosSukup <Lubos.Sukup@kyndryl.com>
2023-08-11 16:49:08 +0200

    secky operatory

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,12 +2,45 @@ const defaultResult= 0;
 let currenctResult = 0;
 
 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNunber){
+   const calcDescription = `${resultBeforeCalc} ${operator}  ${calcNunber}`;
+   outputResult(currenctResult, calcDescription);
+}
+
 function add(){
-   currenctResult = currenctResult + userInput.value;
-   outputResult(currenctResult, '');
+   const initValue = currenctResult;
+   const enteredValue = parseInt(userInput.value);
+   currenctResult = currenctResult + enteredValue;
+   createAndWriteOutput('+' , initValue , enteredValue);
+}
+
+
+function substract(){
+   const initValue = currenctResult;
+   const enteredValue = parseInt(userInput.value);
+   currenctResult = currenctResult - enteredValue;
+   createAndWriteOutput('-' , initValue , enteredValue);
+}
+
+function multiply(){
+   const initValue = currenctResult;
+   const enteredValue = parseInt(userInput.value);
+   currenctResult = currenctResult * enteredValue;
+   createAndWriteOutput('*' , initValue , enteredValue);
+}
+
+function divide(){
+   const initValue = currenctResult;
+   const enteredValue = parseInt(userInput.value);
+   currenctResult = currenctResult / enteredValue;
+   createAndWriteOutput('/' , initValue , enteredValue);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', substract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
 
 
 // ukazat syntax zavolat metody s parametrama bez parametru, ukazat return 
