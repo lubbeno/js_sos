f9cc753890f242600e994898943d273125310823
LubosSukup <Lubos.Sukup@kyndryl.com>
2023-10-18 13:07:59 +0200

    commit

---
+++ II_TPI_01/assets/app.js
@@ -0,0 +1,12 @@
+//alert("prvy alert Lubos Sukup");
+
+let prvaPremenna = 3;
+const druhaPremenna = 7;
+
+prvaPremenna = 20;
+
+const meno = 'Lubos' ;
+
+let vysledokScitania = prvaPremenna + druhaPremenna;
+
+vysledok(meno);
--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,47 +1,69 @@
-const defaultResult= 0;
+const defaultResult = 0;
 let currenctResult = 0;
+let logEntries = new Array();
 
-
-function createAndWriteOutput(operator, resultBeforeCalc, calcNunber){
-   const calcDescription = `${resultBeforeCalc} ${operator}  ${calcNunber}`;
-   outputResult(currenctResult, calcDescription);
+function createAndWriteOutput(operator, resultBeforeCalc, calcNunber) {
+  const calcDescription = `${resultBeforeCalc} ${operator}  ${calcNunber}`;
+  outputResult(currenctResult, calcDescription);
 }
 
-function add(){
-   const initValue = currenctResult;
-   const enteredValue = parseInt(userInput.value);
-   currenctResult = currenctResult + enteredValue;
-   createAndWriteOutput('+' , initValue , enteredValue);
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    cisloPridane: operationNumber,
+    newResult: newResult,
+    time: Date.now(),
+    prdel: function () {
+      return "moja Prdel ma cislo " + currenctResult + " !!";
+    },
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
+function add() {
+  const initValue = currenctResult;
+  const enteredValue = parseInt(userInput.value);
+  currenctResult += enteredValue;
+  createAndWriteOutput("+", initValue, enteredValue);
+  writeToLog('ADD',initValue,enteredValue,currenctResult);
 
-function substract(){
-   const initValue = currenctResult;
-   const enteredValue = parseInt(userInput.value);
-   currenctResult = currenctResult - enteredValue;
-   createAndWriteOutput('-' , initValue , enteredValue);
 }
 
-function multiply(){
-   const initValue = currenctResult;
-   const enteredValue = parseInt(userInput.value);
-   currenctResult = currenctResult * enteredValue;
-   createAndWriteOutput('*' , initValue , enteredValue);
+function substract() {
+  const initValue = currenctResult;
+  const enteredValue = parseInt(userInput.value);
+  currenctResult -= enteredValue;
+  createAndWriteOutput("-", initValue, enteredValue);
+  writeToLog('SUBSTRACT',initValue,enteredValue,currenctResult);
 }
 
-function divide(){
-   const initValue = currenctResult;
-   const enteredValue = parseInt(userInput.value);
-   currenctResult = currenctResult / enteredValue;
-   createAndWriteOutput('/' , initValue , enteredValue);
+function multiply() {
+  const initValue = currenctResult;
+  const enteredValue = parseInt(userInput.value);
+  currenctResult *= enteredValue;
+  createAndWriteOutput("*", initValue, enteredValue);
+  writeToLog('MUSTIPLY',initValue,enteredValue,currenctResult);
 }
 
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', substract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+function divide() {
+  const initValue = currenctResult;
+  const enteredValue = parseInt(userInput.value);
+  currenctResult /= enteredValue;
+  createAndWriteOutput("/", initValue, enteredValue);
+  writeToLog('DIVIDE',initValue,enteredValue,currenctResult);
+}
 
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", substract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
 
-
-// ukazat syntax zavolat metody s parametrama bez parametru, ukazat return 
-// premenne a funkcie sa budu davat na zacatek. 
+// ukazat syntax zavolat metody s parametrama bez parametru, ukazat return
+// premenne a funkcie sa budu davat na zacatek.
