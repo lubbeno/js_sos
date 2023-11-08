6c6b997a2e84c14f89ab4f5efe9f5481b5a918c8
LubosSukup <Lubos.Sukup@kyndryl.com>
2023-11-08 13:10:09 +0100

    11.8.

--- II_TPI_01/assets/app.js
+++ II_TPI_01/assets/app.js
@@ -1,12 +1,43 @@
-//alert("prvy alert Lubos Sukup");
-
 let prvaPremenna = 3;
-const druhaPremenna = 7;
+let druhaPremenna = 7;
 
-prvaPremenna = 20;
+// let vysledok = 3 + 7;
+let vysledok01 = prvaPremenna + druhaPremenna;
 
-const meno = 'Lubos' ;
+//alert("3 + 7");
+//alert(3 + 7);
+//alert("prvaPremenna + druhaPremenna");
+//alert(prvaPremenna + druhaPremenna);
+//alert("vysledok01");
 
-let vysledokScitania = prvaPremenna + druhaPremenna;
+/** 
+let a = 15 + 3 * (4 - 8 / 2);
+let b = ((150.2 / 8) * (18.3 - 20.1)) / 3;
+let c = 3 ** 2 + 8 - 31 / 8;
+let prikladJedna = '15 + 3 * (4-8/2)';
+alert(prikladJedna + ' = ' + a);
+alert(b);
+alert(c);
+*/
 
-vysledok(meno);
+let meno = "Lubos";
+let priezvisko = "Sukup";
+let mojeMeno = meno + " " + priezvisko;
+
+alert(mojeMeno);
+
+let mojaAdresa = "Ulica test jedna 49 ; mesto 11111";
+let adresaSkoly = "Namestie sv. Martina 5; Holic 90851";
+
+
+//-------------------------------
+
+// vidime 8
+//const osem = 8;
+//let a = 15 + 3 * (4-osem/2);
+
+let x = 0;
+
+let y = ((0 + 10) * 3) / 2 - 1;
+
+let textoveZobrazenie = "(0 + 10)*3 /2 -1";
